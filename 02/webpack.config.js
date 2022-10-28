45519dc234ab75106ba9553fbf4e6e74427ea51d
Timur Kovalev <timur1711@iloud.com>
2022-10-28 18:28:22 +0900

    Плагины

--- 02/webpack.config.js
+++ 02/webpack.config.js
@@ -3,31 +3,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
 
 const config = {
-    entry: {
-        about: './src/pages/About.js',
-        home: './src/pages/Home.js',
-    },
     plugins: [
         new HtmlWebpackPlugin(),
         new StatoscopePlugin({
             saveStatsTo: 'stats.json',
             saveOnlyStats: false,
             open: false,
-        }),
+        })
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
     },
-    resolve: {
-        extensions: ['.js']
-    },
     module: {
         rules: [
-            // @TODO js rule
-            // @TODO css rule
+            { test: /\.css$/, use: 'css-loader' },
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel-loader",
+                options:{
+                    presets:[ "@babel/preset-react"]
+                }
+            }
         ],
     },
+    entry: {
+        about: './src/pages/About.js',
+        home: './src/pages/Home.js',
+        index: {import: './src/index.js',
+            dependOn: ['about', 'home']
+        }
+    }
     // @TODO optimizations
     // @TODO lodash treeshaking
     // @TODO chunk for lodash
